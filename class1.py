5ebdcfa0dd1a7a85565f925137006f3fc8998a45
Amrutha-NV <amruthavasudeva28@gmail.com>
2025-05-19 18:10:18 +0530

    class1.py uploaded

---
+++ class1.py
@@ -0,0 +1,8 @@
+print("hello world"); #printing a string in python
+#comment
+#lines of sentence that won't be executed
+#comments are of two types
+#single line comment
+'''
+multi line comment
+'''
